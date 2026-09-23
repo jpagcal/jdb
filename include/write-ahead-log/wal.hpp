6da542fbc1f8f5384d0aedc9e33b18832a4b645b
jpagcal <jpagcal_@outlook.com>
2026-09-22 20:56:11 -0600

    WAL interface

--- include/write-ahead-log/wal.hpp
+++ include/write-ahead-log/wal.hpp
@@ -1,23 +1,30 @@
+#include <fstream>
 #include <string>
 #include <vector>
 
 namespace jdb {
+enum class INST {
+	ups,
+	del,
+};
+
+using ParsedInstruction = std::pair<INST, std::string>;
+
+namespace WriteAheadLogParser {
+	static ParsedInstruction parse_instruction(std::string line);
+	static std::vector<std::string> get_lines(std::fstream fs);
+} // WriteAheadLogParser
+
 class WriteAheadLog {
 public:
-	// we want to assume we already have a given file open, which is why I want the WAL to take in 
-	// some path to some file, but what if it isnt?
-	// we can let the parent program specify the path (it needs to be a valid path) and then pass it in as a parameter
-	// invalid paths throw a runtime error; if there is no file at that path then the constructor creates one 
-	// a WALs job is just to append instructions to a log and to replay instructions. 
-	// instructions are in the form of sometihng along the lines of 'INST key[.val]'
-	// qs:
-	// 	how should we handle malformed logs?
-	// 	failure during replay?
-	void append(std::string inst) const;
-	void replay(std::vector<std::string> insts);
+	WriteAheadLog();
+	~WriteAheadLog();
+
+	void append(std::string line_to_append) const;
+	std::vector<ParsedInstruction> instructions() const;
 	
 private:
-	int fd_;
+	std::fstream fs_;
 	// needs a parser
 };
 } //jdb
